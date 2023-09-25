776d74ad55c76b6ab04be82731a6e48f28d494c5
graceword <graceword@qq.com>
2023-09-25 20:33:49 +0800

    imge url

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { getAssetURL } from "../utils/get-asset-url";
+import { getAssetURL, getAssetPrefix } from "../utils/get-asset-url";
 
 export default function Article({ article, bordered }) {
   return (
